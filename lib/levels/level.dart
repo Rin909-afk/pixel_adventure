83046028b33a4173783c0e0c5d9ccc0763adae94
Rin <rbztv3@gmail.com>
2025-01-27 09:55:53 +0700

    First commit

---
+++ lib/levels/level.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
+
+class Level extends World {
+  final String levelName;
+  Level({required this.levelName});
+  late TiledComponent level;
+
+  @override
+  FutureOr<void> onLoad() async {
+    level = await TiledComponent.load('$levelName.tmx', Vector2.all(16));
+
+    add(level);
+
+    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+
+    for (final spawnPoint in spawnPointsLayer!.objects) {
+      switch (spawnPoint.class_) {
+        case 'Player':
+          final player = Player(
+            character: 'Pink Man',
+            position: Vector2(spawnPoint.x, spawnPoint.y),
+          );
+          add(player);
+          break;
+        default:
+      }
+    }
+
+    add(
+      Player(
+        character: 'Pink Man',
+      ),
+    );
+
+    return super.onLoad();
+  }
+}
